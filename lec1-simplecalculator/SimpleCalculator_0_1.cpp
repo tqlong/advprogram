e2a9488e0e9b232089b2c069b716a5434df6c1e6
Long <tqlong@gmail.com>
2017-01-14 17:21:48 +0700

    fixed indentation

--- lec1-simplecalculator/SimpleCalculator_0_1.cpp
+++ lec1-simplecalculator/SimpleCalculator_0_1.cpp
@@ -4,34 +4,35 @@ using namespace std;
 
 int main()
 {
-	int num1, num2;
-	char op;
+    int num1, num2;
+    char op;
 
-	cin >> num1 >> op >> num2;
-	switch (op) {
-		case '+':
-			cout << num1 + num2 << endl;
-			break;
-		case '-':
-			cout << num1 - num2 << endl;
-			break;
-		case '*':
-			cout << num1 * num2 << endl;
-			break;
-		case '/':
-		    if (num2 == 0)
-                cout << "Invalid divisor" << endl;
-            else
-                cout << num1 / num2 << endl;
-			break;
-		case '%':
-			if (num2 == 0)
-                cout << "Invalid divisor" << endl;
-            else
-                cout << num1 % num2 << endl;
-			break;
-		default:
-			cout << "Invalid operator" << endl;
-	}
-	return 0;
+    cin >> num1 >> op >> num2;
+    switch (op)
+    {
+    case '+':
+        cout << num1 + num2 << endl;
+        break;
+    case '-':
+        cout << num1 - num2 << endl;
+        break;
+    case '*':
+        cout << num1 * num2 << endl;
+        break;
+    case '/':
+        if (num2 == 0)
+            cout << "Invalid divisor" << endl;
+        else
+            cout << num1 / num2 << endl;
+        break;
+    case '%':
+        if (num2 == 0)
+            cout << "Invalid divisor" << endl;
+        else
+            cout << num1 % num2 << endl;
+        break;
+    default:
+        cout << "Invalid operator" << endl;
+    }
+    return 0;
 }
