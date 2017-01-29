7e8ca3f59e0e21fc9054d8478bec7cce2913e2d2
Long <tqlong@gmail.com>
2017-01-29 16:21:27 +0700

    chooseWord() random 3 words

--- lec3-hangman/HangMan.cpp
+++ lec3-hangman/HangMan.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 using namespace std;
 
+int generateRandomNumber(int min, int max)
+{
+    return rand() % (max-min) + min;
+}
+
 string chooseWord()
 {
-    return "choose";
+    const int NUMBER_OF_WORDS = 3;
+    string wordList[] = { "dog", "cat", "human" };
+    return wordList[generateRandomNumber(0, NUMBER_OF_WORDS)];
 }
 
 int main()
