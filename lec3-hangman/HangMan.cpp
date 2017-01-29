5a8a3bea84e235af6bdba293366d91e1c61f71fb
Long <tqlong@gmail.com>
2017-01-29 16:08:02 +0700

    chooseWord() template

---
+++ lec3-hangman/HangMan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string chooseWord()
+{
+    return "choose";
+}
+
+int main()
+{
+    cout << "chooseWord() returns: " << chooseWord() << endl;
+
+    return 0;
+}
