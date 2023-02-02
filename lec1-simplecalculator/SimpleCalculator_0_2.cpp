47c2d489c3dc5f4ee9293ea8ea39b00278a65ef8
nvicuong <112748853+nvicuong@users.noreply.github.com>
2023-02-02 17:39:22 +0700

    Add files via upload

--- lec1-simplecalculator/SimpleCalculator_0_2.cpp
+++ lec1-simplecalculator/SimpleCalculator_0_2.cpp
@@ -1,25 +1,44 @@
 #include <iostream>
 #include <cstdlib>
-
+#include <math.h>
+#include <cmath>
+#include <string>
 using namespace std;
 
-int arithmetic(int num1, int num2, char op);
+double arithmetic(double num1, double num2, char op);
 
 int main(int argc, char* argv[])
 {
-    int num1, num2, result;
+    double num1, num2, result;
     char op;
-
-    num1 = atoi(argv[1]);
+    if (argc == 3)
+    {
+        string s;
+        s = argv[1];
+        num1 = atof(argv[2]);
+        if (s == "cos")
+            cout << cos(num1);
+        if (s == "sqrt")
+            cout << sqrt(num1);
+        if (s != "cos" && s != "sqrt")
+            cout << "tham so khong hop le!";
+        exit(1);
+    }
+    if (argc != 3 && argc != 4)
+    {
+        cout << "so tham so khong hop le!";
+        exit(1);
+    }
+    num1 = atof(argv[1]);
     op = argv[2][0];
-    num2 = atoi(argv[3]);
+    num2 = atof(argv[3]);
 
     cout << arithmetic(num1, num2, op) << endl;
 
     return 0;
 }
 
-int arithmetic(int num1, int num2, char op)
+double arithmetic(double num1, double num2, char op)
 {
     switch (op)
     {
@@ -38,13 +57,13 @@ int arithmetic(int num1, int num2, char op)
         else
             return num1 / num2;
     case '%':
-        if (num2 == 0)
+        if (num1 != (int)num1 || num2 != (int)num2 || num2 == 0)
         {
             cout << "Invalid divisor" << endl;
             exit(1);
         }
         else
-            return num1 % num2;
+            return (int)num1 % (int)num2;
     default:
         cout << "Invalid operator" << endl;
         exit(1);
