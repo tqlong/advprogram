b40bee7248c11894e1781d4e39e5bd0f68925060
Phu nguyen 22021138 <phunguyen.it.k67@gmail.com>
2023-02-05 17:54:01 +0700

    Phu Nguyen 22021138

--- lec1-simplecalculator/SimpleCalculator_0_2.cpp
+++ lec1-simplecalculator/SimpleCalculator_0_2.cpp
@@ -1,51 +1,102 @@
-#include <iostream>
-#include <cstdlib>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int arithmetic(int num1, int num2, char op);
+double arithmetic (double num1, double num2, char op);
+double getFunc (string func, double num1);
 
-int main(int argc, char* argv[])
-{
-    int num1, num2, result;
+int main (int argc, char* argv[]) {
+    double num1, num2, result;
+    string func;
     char op;
 
-    num1 = atoi(argv[1]);
-    op = argv[2][0];
-    num2 = atoi(argv[3]);
+    if (argc == 4) {
+        stringstream s (argv[1]);
+        s >> num1;
+        op = argv[2][0];
+        stringstream s2 (argv[3]);
+        s2 >> num2;
 
-    cout << arithmetic(num1, num2, op) << endl;
+        result = arithmetic(num1, num2, op);
 
-    return 0;
-}
+        cout << result << endl;
 
-int arithmetic(int num1, int num2, char op)
-{
-    switch (op)
-    {
-    case '+':
+        return 0;
+    } else if (argc == 3) {
+        func = argv[1];
+        stringstream s (argv[2]);
+        s >> num1;
+
+        result = getFunc(func, num1);
+
+        cout << result << endl;
+
+        return 0;
+    } else {
+        cout << "Invalid parameters";
+        exit(1);
+    }
+} 
+
+double arithmetic (double num1, double num2, char op) {
+    if (op == '+') {
         return num1 + num2;
-    case '-':
+    } else if (op == '-') {
         return num1 - num2;
-    case 'x':
+    } else if (op == 'x') {
         return num1 * num2;
-    case '/':
-        if (num2 == 0)
-        {
+    } else if (op == '/') {
+        if (num2 == 0) {
             cout << "Invalid divisor" << endl;
             exit(1);
         }
         else
             return num1 / num2;
-    case '%':
-        if (num2 == 0)
-        {
+    } else if (op == '%') {
+        if (num2 == 0) {
             cout << "Invalid divisor" << endl;
             exit(1);
+        } else {
+            if ((int)num1 == num1 && (int) num2 == num2) {
+                return (int)num1 % (int)num2;
+            } else {
+                cout << "Modulo can't be used on real number" << endl;
+                exit(1);
+            }
         }
-        else
-            return num1 % num2;
-    default:
+    } else {
+        cout << "Invalid operator" << endl;
+        exit(1);
+    }
+}
+
+double getFunc (string func, double num1) {
+    if (func == "cos") {
+        return cos (num1);
+    } else if (func == "sin") {
+        return sin (num1);
+    } else if (func == "tan") {
+        if (cos(num1) == 0) {
+            cout << "Invalid parameters" << endl;
+            exit (1);
+        } else {
+            return tan (num1);
+        }
+    } else if (func == "cot") {
+        if (sin(num1) == 0) {
+            cout << "Invalid parameters" << endl;
+            exit(1);
+        } else {
+            return cos(num1) / sin(num1);
+        }
+    } else if (func == "sqrt") {
+        if (num1 < 0) {
+            cout << "Invalid parameters" << endl;
+            exit(1);
+        } else {
+            return sqrt(num1);
+        }
+    } else {
         cout << "Invalid operator" << endl;
         exit(1);
     }
