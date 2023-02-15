c41b8c6064c4657751843b0cba4c836948fb6c22
TVAexe <124753939+TVAexe@users.noreply.github.com>
2023-02-15 21:32:56 +0700

    Update SimpleCalculator_0_1.cpp

--- lec1-simplecalculator/SimpleCalculator_0_1.cpp
+++ lec1-simplecalculator/SimpleCalculator_0_1.cpp
@@ -8,31 +8,14 @@ int main()
     char op;
 
     cin >> num1 >> op >> num2;
-    switch (op)
-    {
-    case '+':
-        cout << num1 + num2 << endl;
-        break;
-    case '-':
-        cout << num1 - num2 << endl;
-        break;
-    case '*':
-        cout << num1 * num2 << endl;
-        break;
-    case '/':
-        if (num2 == 0)
-            cout << "Invalid divisor" << endl;
-        else
-            cout << num1 / num2 << endl;
-        break;
-    case '%':
-        if (num2 == 0)
-            cout << "Invalid divisor" << endl;
-        else
-            cout << num1 % num2 << endl;
-        break;
-    default:
-        cout << "Invalid operator" << endl;
-    }
+    
+    if (op=='+') {cout << num1+num2;}
+    if (op=='-') {cout << num1-num2;}
+    if (op=='*')  {cout << num1*num2;}
+    if (op=='/' && num2!=0) {cout << num1/num2;}
+    if (op=='%' && num2!=0) {cout << num1%num2;}
+    if (op!='+' && op!='-' && op!='*' && op!='/' && op !='%') {cout << "Invalid operator";}
+    if (op=='/' && num2==0) {cout << "Invalid divisor";}
+    if (op=='%' && num2==0) {cout << "Invalid divisor";}
     return 0;
 }
